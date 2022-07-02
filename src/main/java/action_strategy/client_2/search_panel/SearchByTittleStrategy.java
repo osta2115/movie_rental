56b2586d8618d0f2cf3e94bb47140e428be4824c
RafSzef <raf.szefler@gmail.com>
2022-07-02 11:24:50 +0200

    Search start

--- src/main/java/action_strategy/client_2/search_panel/SearchByTittleStrategy.java
+++ src/main/java/action_strategy/client_2/search_panel/SearchByTittleStrategy.java
@@ -1,12 +1,32 @@
 package action_strategy.client_2.search_panel;
 
 import action_strategy.Strategy;
+import action_strategy.StrategyCommons;
+import hibernate.ProductRepositoryHibernate;
+import lombok.extern.slf4j.Slf4j;
+import tables.Product;
 
+import java.util.List;
+import java.util.Scanner;
+
+@Slf4j
 public class SearchByTittleStrategy implements Strategy {
+    Scanner scanner = new Scanner(System.in);
     @Override
     public void algorithm() {
-        //TODO
-        System.out.println("search by title method");
+
+        System.out.println("Enter product name:");
+        String title = scanner.nextLine();
+
+        ProductRepositoryHibernate repository = StrategyCommons.getInstance().getProductRepositoryHibernate();
+        List<Product> resultList = repository.getListOfProductWithGivenTitle(title);
+        if (resultList.isEmpty()){
+            System.out.println("No product with given title. \nReturning to previous menu");
+            new SearchPanelLogic().startAdminUserManagementPanel();
+        }
+        resultList.forEach(System.out::println);
+        System.out.println("Do you want to rent this ?");
+
         new SearchPanelLogic().startAdminUserManagementPanel();
     }
 }
